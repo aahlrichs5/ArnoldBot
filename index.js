d0835dfc397690b2224e0afa8730090bd167544f
ahlrichsa <aahlrichs5@gmail.com>
2020-06-16 22:40:52 -0500

    Created the initial bot with a test and play command that will join and play music from a youtube link Not included in package is shown in the gitignore

---
+++ index.js
@@ -0,0 +1,70 @@
+const Discord = require("discord.js");
+const TOKEN = require("./config.json");
+const bot = new Discord.Client();
+
+let servers = [];
+const ytdl = require("ytdl-core", "ffmpeg-binaries"); // for the youtube links to be converted to audio
+
+// Logging when bot is online
+bot.once("ready", () => {
+  console.log("Ready");
+});
+
+bot.login(TOKEN.token); // logs in with the token
+
+// Test funtion when !test is written the bot will reply
+bot.on("message", (message) => {
+  if (message.content.startsWith(`${TOKEN.prefix}test`)) {
+    message.channel.send("beep boop");
+  }
+});
+
+// This function is provoced when !play is commanded and the bot will play a youtube link in the voice channel
+bot.on("message", (message) => {
+  let args = message.content.substring(TOKEN.prefix.length).split(" ");
+
+  switch (args[0]) {
+    case "play":
+      function play(connection, message) {
+        let server = servers[message.guild.id];
+
+        server.dispatcher = connection.play(
+          ytdl(server.queue[0], { filter: "audioonly" })
+        );
+
+        server.queue.shift();
+        server.dispatcher.on("end", function () {
+          if (server.queue[0]) play(connection, message);
+          else connection.disconnect();
+        });
+      }
+
+      if (!args[1]) {
+        message.channel.send(
+          "You need to provide a link for me to play a song."
+        );
+        return;
+      }
+      if (!message.member.voice.channel) {
+        message.channel.send(
+          "You must be in a voice channel to request a song."
+        );
+        return;
+      }
+
+      if (!servers[message.guild.id]) {
+        servers[message.guild.id] = {
+          queue: [],
+        };
+      }
+      let server = servers[message.guild.id];
+
+      server.queue.push(args[1]);
+
+      if (!message.member.voice.connection)
+        message.member.voice.channel.join().then(function (connection) {
+          play(connection, message);
+        });
+      break;
+  }
+});
