081f912b0028b543aa1320510c2bbee2d14f7854
Alex Ahlrichs <aahlrichs5@gmail.com>
2023-05-15 21:44:08 -0500

    Update bot to fetch all guild ids rather than coding for a specific server

--- src/slash/on-ready.ts
+++ src/slash/on-ready.ts
@@ -1,21 +1,27 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
 import { CommandList } from "./command-list";
 const TOKEN = require("../../config.json");
+
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
 
 export const onReady = async (bot: Client) => {
   const rest = new REST({ version: "10" }).setToken(TOKEN.token);
 
   const commandData = CommandList.map((command) => command.data.toJSON());
 
-  await rest.put(
-    Routes.applicationGuildCommands(
-      bot.user?.id || "missing id",
-      TOKEN.guildId
-    ),
-    { body: commandData }
-  );
+  const guildList = bot.guilds.cache.map((guild) => guild.id);
+  var guilds;
+
+  for (const guildId of guildList) {
+    guilds = await rest.put(
+      Routes.applicationGuildCommands(bot.user?.id || "missing id", guildId),
+      { body: commandData }
+    );
+  }
 
   console.log("Ready Slash Commands");
 };
